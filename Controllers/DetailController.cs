d9c3850b72b59c4e32ddf60e469bb78be49a70a9
Be1t0k <pasha.moshckov@yandex.ru>
2023-01-02 17:59:36 +0300

    add basic creation detail and technic

---
+++ Controllers/DetailController.cs
@@ -0,0 +1,31 @@
+﻿using CourseWorkFactoryAutomatization.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace CourseWorkFactoryAutomatization.Controllers
+{
+    public class DetailController : Controller
+    {
+        private CourseWorkContext workContext;
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public DetailController(CourseWorkContext courseWorkContext)
+        {
+            workContext = courseWorkContext;
+        }
+        [HttpGet]
+        public IActionResult CreateDetail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateDetail(Detail d)
+        {
+            workContext.Details.Add(d);
+            workContext.SaveChanges();
+            return Redirect("/");
+        }
+    }
+}
---
+++ Controllers/TechnicController.cs
@@ -0,0 +1,31 @@
+﻿using CourseWorkFactoryAutomatization.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseWorkFactoryAutomatization.Controllers
+{
+    public class TechnicController : Controller
+    {
+        private CourseWorkContext workContext;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public TechnicController(CourseWorkContext courseWorkContext)
+        {
+            workContext = courseWorkContext;
+        }
+        [HttpGet]
+        public IActionResult CreateTechnic()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateTechnic(Technic t)
+        {
+            workContext.Technics.Add(t);
+            workContext.SaveChanges();
+            return Redirect("/");
+        }
+    }
+}

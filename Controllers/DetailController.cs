c4040ad77a3db38638f094151e84eccb4a50f971
Be1t0k <pasha.moshckov@yandex.ru>
2023-01-03 21:44:30 +0300

    create all crud methods for det and tech

--- Controllers/DetailController.cs
+++ Controllers/DetailController.cs
@@ -1,12 +1,17 @@
 ﻿using CourseWorkFactoryAutomatization.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace CourseWorkFactoryAutomatization.Controllers
 {
     public class DetailController : Controller
     {
         private CourseWorkContext workContext;
+        
+        
         public IActionResult Index()
         {
             return View();
@@ -18,6 +23,8 @@ namespace CourseWorkFactoryAutomatization.Controllers
         [HttpGet]
         public IActionResult CreateDetail()
         {
+            IEnumerable<Technic> technics = workContext.Technics.ToList();
+            ViewBag.Technics = new SelectList(technics, "Id", "Title");
             return View();
         }
         [HttpPost]
@@ -25,29 +32,50 @@ namespace CourseWorkFactoryAutomatization.Controllers
         {
             workContext.Details.Add(d);
             workContext.SaveChanges();
-            return Redirect("/");
+            return Redirect("/Detail");
         }
 
         public IActionResult GetDetails()
         {
             using (var context = workContext) 
             {
-                return View(context.Details.ToList());
+                return View(context.Details.Include(u => u.Technic).ToList());
+            }
+        }
+        public IActionResult EditDetail(Detail d)
+        {
+            using (var context = workContext)
+            {
+                context.Entry(d).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+            return Redirect("/GetDetails");
+        }
+        [HttpGet]
+        public IActionResult EditDetail(int id)
+        {
+            
+            using (var context = workContext)
+            {
+                IEnumerable<Technic> technics = context.Technics.ToList();
+                ViewBag.Technics = new SelectList(technics, "Id", "Title");
+                return View(context.Details.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+        [HttpGet]
+        public IActionResult InfoDetails(int id)
+        {
+            using (var context = workContext)
+            {
+                return View(context.Details.Where(x => x.Id == id).FirstOrDefault());
             }
         }
 
-
-        /*[HttpPost]
-        public IActionResult DeleteDetail(int id) {
-            var delDet = workContext.Details.Where(d => d.Id == id).First();
-            workContext.Details.Remove(delDet);
+        public IActionResult DeleteDetail(int id, Detail detail) {
+            detail = workContext.Details.Where(d => d.Id == id).First();
+            workContext.Details.Remove(detail);
             workContext.SaveChanges();
             return Redirect("/");
         }
-        [HttpGet]
-        public IActionResult DeleteDetial()
-        {
-            return View();
-        }*/
     }
 }
--- Controllers/TechnicController.cs
+++ Controllers/TechnicController.cs
@@ -1,5 +1,7 @@
 ﻿using CourseWorkFactoryAutomatization.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace CourseWorkFactoryAutomatization.Controllers
 {
@@ -15,15 +17,63 @@ namespace CourseWorkFactoryAutomatization.Controllers
         {
             workContext = courseWorkContext;
         }
+        //Create-Get
         [HttpGet]
         public IActionResult CreateTechnic()
         {
             return View();
         }
+        //Create-Post
         [HttpPost]
         public IActionResult CreateTechnic(Technic t)
         {
             workContext.Technics.Add(t);
+            workContext.SaveChanges();
+            return Redirect("/");
+        }
+        //GetTech-Get
+        public IActionResult GetTechnics()
+        {
+            using (var context = workContext)
+            {
+                return View(context.Technics.ToList());
+            }
+        }
+        //Info-Get
+        [HttpGet]
+        public IActionResult InfoTechnics(int id)
+        {
+
+            using (var context = workContext)
+            {
+                return View(context.Technics.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+        //Edit-Post
+        public IActionResult EditTechnic(Technic t)
+        {
+            using (var context = workContext)
+            {
+                context.Entry(t).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+            return Redirect("/GetTechnics");
+        }
+        //Edit-Get
+        [HttpGet]
+        public IActionResult EditTechnic(int id)
+        {
+            using (var context = workContext)
+            {
+                return View(context.Technics.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        //Delete-Post
+        public IActionResult DeleteTechnic(int id, Technic technic)
+        {
+            technic = workContext.Technics.Where(d => d.Id == id).First();
+            workContext.Technics.Remove(technic);
             workContext.SaveChanges();
             return Redirect("/");
         }
